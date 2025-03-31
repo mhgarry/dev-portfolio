ee195a1dd217f99392cdca56a1ae856033f852e4
Matthew Garry <jinx@Matthews-MacBook-Air.local>
2025-03-30 22:19:57 -0400

    bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
     "Node.js",
     "Backend Developer",
     "Frontend Developer",
-    "github"
+    "github",
     "Typescript",
     "Javascript",
     "wordpress",
@@ -79,7 +79,7 @@ export default function RootLayout({
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
-          fontSans.variable
+          fontSans.variable,
         )}
       >
         <ThemeProvider attribute="class" defaultTheme="light">
